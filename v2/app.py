b45e4a5141323fdb346219c125aa1746ba9993c5
Felipe Rubin <felipe.rubin@acad.pucrs.br>
2019-07-18 15:02:53 -0300

    parsed files

--- v2/app.py
+++ v2/app.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 
-# import argparse #install
+import argparse #install
 import sys
 import os
 import requests #install
@@ -31,6 +31,8 @@ class Teacher:
 	def __repr__(self):
 		return "Teacher()"
 
+# window.open('https://moodle.pucrs.br/mod/resource/view.php?id=1285167&redirect=1'); return false;
+# class Topic
 
 # Course Information
 class Course:
@@ -60,7 +62,7 @@ class Course:
 
 
 # Peek link to see if there's anything interesting
-def peek_link():
+def peek_link(br,link):
 	return 0
 # Returns List of Courses
 def find_courses(br):
@@ -131,14 +133,145 @@ def write_file(path,data):
 #activity label modtype_label 
 #module-1413106
 # Download a course content to the specific path
-def download_course_content(path,course_content):
+# Text as markdown
+# class Activity:
+# 	def __init__(self,atype,links=[],text):
+# 		self.atype = atype
+# 		self.links = links
+# 		self.text = text
+# 	def __str__(self):
+# 		return 'Not Implemented Yet'
+
+
+
+
+
+class ActivityParser:
+	def __init__(self):
+		types = ['activity label modtype_label ',
+		'activity resource modtype_resource ',
+		'activity label modtype_label ',
+		'activity url modtype_url ',
+		'activity page modtype_page ',
+		'activity assign modtype_assign ',
+		]
+	# Converts HTML to Github Markdown
+	def html2md(self,text=[]):
+		outmd = ''
+		# for t in text:
+		# 	if t.name == 'h1':
+		# 		outmd += t.string+'#'
+		# 	elif t.name == 'h2':
+		# 		outmd += t.string+'##'
+		# 	elif t.name == 'h3':
+		# 		outmd += t.string+'###'
+		# 	elif t.name == 'h4':
+		# 		outmd += t.string+'####'
+		# 	elif t.name == 'h5':
+		# 		outmd += t.string+'#####'
+		# 	elif t.name == 'h6':
+		# 		outmd += t.string+'######'
+		# 	elif t.name == 'p':
+		return ''
+
+	def download_activity(self,activity):
+		return 0		
+
+
+class Resource:
+	def __init__(self,name,link):
+		self.name = name
+		self.link = link
+	def __str__(self):
+		return str(self.name) + ' '+str(self.link)
+# Parser responsible for parsing a section content
+class SectionParser:
+	# def __init__(self):
+		# return 0
+
+	def find_name(self,section):
+		name = ''
+		# print('Section Name',section['aria-label'])
+		# First try
+		name = section['aria-label'] #1st try
+		if name == ' ':  # 2nd try
+			name = section.find('span',{'class':{'hidden sectionname'}}).string
+			if name == ' ': # 3rd try
+				name = section.find('div',{'class':{'content'}})\
+				.find('h3',{'class':{'sectionname'}})\
+				.find('span').string
+				if name is None or name == '' or name == ' ':
+					name = section['id']
+		return name
+	def find_summary(self,section):
+		# return = ct.find('div',{'class':{'summary'}})
+		return section.find('div',{'class':{'content'}}).find('div',{'class':{'summary'}})
+	def find_modules(self,section):
+		return course_content.findAll('li', id=re.compile('^module-'))
+	def find_files(self,section): #Find all the files
+		print("##LINKS###")
+		print(section.links())
+		print("##########")
+	# Finds resources such as PDFs, zip files and more
+	def find_resources(self,section):
+		resources = []
+		modresources = section.findAll('li',{'class':{'activity resource modtype_resource'}})
+		for resource in modresources:
+
+			res =  resource.find('div',{'class':{'activityinstance'}})
+			name = res.a.find('span',class_='instancename').contents[0]
+			link = res.a['href']
+			resources.append(Resource(name,link))
+		return resources
+	def find_actual_resource(self,section):
+		workaround = section.find('div',{'class':{'resourceworkaround'}})
+		if workaround is not None:
+			return workaround.a['href']
+		else:
+			# section.find('object',{'id':{'resourceworkaround'}})
+			return section.find('object',id='resourceobject')['data']
+
+			
+	def find_urls(self,section):
+		#activity url modtype_url 
+		# instancename
+		urls = []
+		return 0
+
+	def __str__(self):
+		return 'NOREPRESENTATION'
+
+
+def parse_content_sections(course_content):
+	return course_content.findAll('li', id=re.compile('^section-'))
+
+def download_course_content(path,course_content,br):
 	# Get each content
-	course_topics = course_content.ul
-	for topic in course_topics:
-		print(topic['id'])
-
-
-
+	# course_topics = course_content.ul
+	# for topic in course_topics:
+		# print(topic['id'])
+	sections = parse_content_sections(course_content)
+	print('##Sections##')
+	# print(sects)
+	sc = SectionParser()
+	current_url = br.get_url()
+	for section in sections:
+		print("##NAME##")
+		print(sc.find_name(section))
+		print("##SUMMARY##")
+		print(sc.find_summary(section))
+		print("##RESOURCES##")
+		for res in sc.find_resources(section):
+			print('Finding Actual Resource for link: ',res.link)
+			br.open(res.link)
+			if br.get_url() != res.link:
+				res.link = br.get_url()
+			else:
+				res.link = sc.find_actual_resource(br.get_current_page())	
+			print('Resource: ',res)
+		print("#############")
+		# sc.find_files()
+	print('############')
 
 	return 0
 # Downloads a course
@@ -154,7 +287,7 @@ def download_course(br,course,rootdir):
 		pass
 	else:
 		# There's only a single page
-		download_course_content(rootdir+'/'+str(course.name),content)
+		download_course_content(rootdir+'/'+str(course.name),content,br)
 
 	# print(course_content)
 	return 0
